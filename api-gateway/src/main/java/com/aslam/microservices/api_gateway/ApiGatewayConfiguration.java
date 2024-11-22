15f15355719fd8789c793fb044ede2498531b0b8
Aslam Ansari <aansari@quark.com>
2024-11-22 16:00:57 +0530

    IN API gateway removed service name from url which passing using api gateway

---
+++ api-gateway/src/main/java/com/aslam/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,30 @@
+package com.aslam.microservices.api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    @Bean
+    public RouteLocator getewayRouter(RouteLocatorBuilder builder) {
+//        Function<PredicateSpec, Buildable<Route>> routeFuction
+//                = p -> p.path("/get")
+//                .filters(f -> f.addRequestHeader("myHeader", "myURI"))
+//                .uri("http://httpbin.org:80");
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("myHeader", "myURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
